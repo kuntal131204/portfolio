b2e13c7612b9b7ce865174a3ceafeee884485a8a
KUNTAL GHOSH <business.teamkuntal@gmail.com>
2026-06-06 09:00:30 +0530

    Update social.ts

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,7 +1,7 @@
 export const social = [
-  { url: "mailto:business.teamkuntal@gmail.com", name: "mail" },
+  { url: "mailto:hello.kuntalghosh.in", name: "mail" },
   { url: "https://github.com/kuntal131204", name: "github" },
   { url: "https://www.linkedin.com/in//kuntalghosh-bytekicker/", name: "linkedin" },
   { url: "https://x.com/unscripted_lens", name: "x" },
-  //{ url: "https://www.instagram.com/davidhckh/", name: "instagram" },
+  { url: "https://www.instagram.com/_bytekicker/", name: "instagram" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
