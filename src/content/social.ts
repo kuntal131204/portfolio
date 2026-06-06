33440323cfc16fa705819cfd384b1353fe86912d
KUNTAL GHOSH <business.teamkuntal@gmail.com>
2026-06-06 09:25:31 +0530

    Update social.ts

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,5 +1,5 @@
 export const social = [
-  { url: "mailto:hello.kuntalghosh.in", name: "mail" },
+  { url: "mailto:hello@kuntalghosh.in", name: "mail" },
   { url: "https://github.com/kuntal131204", name: "github" },
   { url: "https://www.linkedin.com/in//kuntalghosh-bytekicker/", name: "linkedin" },
   { url: "https://x.com/unscripted_lens", name: "x" },
